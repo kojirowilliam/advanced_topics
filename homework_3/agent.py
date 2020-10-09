394df2fa94fa6864dcef8caa38c1ed4c93470af7
KY <kyamada20@geffenacademy.ucla.edu>
2020-10-09 02:54:32 -0700

    made it match the homework requirements.

--- homework_3/agent.py
+++
@@ -1,9 +0,0 @@
-class Agent():
-    def __init__(self, percept):
-        self.percept = percept
-        rule(percept)
-
-    def rule(self):
-
-        # Agent doesn't move, but tells the environment to move the agent in the environment.
-        pass
