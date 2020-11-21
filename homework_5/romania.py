f6d869956f15fc6f73f7e802cf2ab1e969772417
0hq <will@depue.net>
2020-11-20 21:49:43 -0800

    finished everything

--- homework_5/romania.py
+++ homework_5/romania.py
@@ -66,37 +66,28 @@ class Problem:
     __init__, goal_test, and path_cost. Then you will create instances
     of your subclass and solve them with the various search functions."""
 
-    def connections(self, state):
-        print("\tState")
-        print("\t\t" + state)
-        print("\t\t" + str(self.tree[state]))
-        return self.tree[state]
-
-    def actions(self, state):
+    def actions(self, state, log=True):
         """Return the actions that can be executed in the given
         state. The result would typically be a list, but if there are
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
 
-        list = []
+        if log:
+            print("\tState")
+            print("\t\t" + str(state))
+            print("\t\t" + str(self.tree[state]))
 
-        for i in self.connections(state):
-            list.append(i[0])
+            print("\tActions")
+            print("\t\t" + str([i[0] for i in self.tree[state]]))
 
-        print("\tActions")
-        print("\t\t" + str(list))
-
-        if list is None:
-            raise NotImplementedError
-
-        return list
+        return [i[0] for i in self.tree[state]]
 
     def result(self, state, action):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state)."""
 
-        if action in self.actions(state):
+        if action in self.actions(state, False):
             return action
 
         return NotImplementedError
@@ -128,11 +119,38 @@ class BreadthRomania(Problem):
 
         return state == self.goal
 
-        # if isinstance(self.goal, list):
-        #     return is_in(state, self.goal)
-        # else:
-        #     return state == self.goal
 
+class UniformCostRomania(Problem):
+
+    def __init__(self, initial, search_space, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
+        self.initial = initial
+        self.goal = goal
+        self.tree = search_space
+
+    def path_cost(self, c, state1, action, state2):
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2. If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+
+        for i in self.tree[state1]:
+            if i[0] == state2:
+                return i[1] + c
+
+        raise NotImplementedError
+
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+
+        return state == self.goal
 
 
 # ----------------------------------------------------------------------------
@@ -164,7 +182,7 @@ class Node:
 
     def expand(self, problem):
         """List the nodes reachable in one step from this node."""
-
+        loop1 = 0
         list = []
 
         for actions in problem.actions(self.state):
@@ -208,6 +226,7 @@ def breadth_first_search(problem):
     node : Node class
     A node with state 'goal_state' and the 'path' from the 'problem.initial' to the 'goal_state'.
     '''
+    loop = 0
 
     main_node = Node(problem.initial)
     if problem.goal_test(main_node.state):
@@ -215,13 +234,59 @@ def breadth_first_search(problem):
     frontier = [main_node]
     reached = [problem.initial]
     while frontier:
-        print("New Loop")
-        print(" " + str(frontier))
+        loop += 1
+        print("Current Frontier: " + str(frontier))
         main_node = frontier.pop(0)
-        print(" " + str(main_node))
+        print("New Node Expansion: " + str(loop))
+        print("Popped Frontier: " + str(frontier))
+        print("Main Node: " + str(main_node))
         for child in main_node.expand(problem):
-            print("\t\tChild")
-            print("\t\t\t" + str(child))
+            print("\tChild")
+            print("\t\t" + str(child))
+            s = child.state
+            if problem.goal_test(s):
+                print("GOAL STATE")
+                return child
+            if s not in reached:
+                print("Not reached, appending")
+                reached.append(s)
+                frontier.append(child)
+        print('. \n.')
+
+def uniform_cost_search(problem):
+    '''
+    Returns a solution to the problem or returns failure.
+    Parameters
+    ----------
+    state : list
+    The initial state of the environment.
+    Returns
+    -------
+    node : Node class
+    A node with state 'goal_state' and the 'path' from the 'problem.initial' to the 'goal_state'.
+    '''
+
+    loop = 0
+
+    main_node = Node(problem.initial)
+    if problem.goal_test(main_node.state):
+        return main_node
+    frontier = [main_node]
+    reached = [problem.initial]
+    while frontier:
+        loop += 1
+        print("Unsorted Frontier: " + str(frontier))
+        print([key_path(nodes) for nodes in frontier])
+        frontier.sort(key=key_path)
+        print("Sorted Frontier: " + str(frontier))
+        print([key_path(nodes) for nodes in frontier])
+        main_node = frontier.pop(0)
+        print("New Node Expansion: " + str(loop))
+        print("Popped Frontier: " + str(frontier))
+        print("Main Node: " + str(main_node))
+        for child in main_node.expand(problem):
+            print("\tChild")
+            print("\t\t" + str(child))
             s = child.state
             if problem.goal_test(s):
                 print("GOAL STATE")
@@ -233,9 +298,21 @@ def breadth_first_search(problem):
         print('. \n.')
 
 
+def key_path(child_node):
+    return child_node.path_cost
+
+
 if __name__ == "__main__":
-    solution = breadth_first_search(BreadthRomania("Arad", base_tree, "Bucharest"))
-    # print(solution.get_path())
-    # for nodes in solution.get_path():
-    #     print(nodes.get_state())
-    # print(solution.get_state())
+    solution_breadth = breadth_first_search(BreadthRomania("Arad", base_tree, "Bucharest"))
+    # print(solution_breadth.path)
+
+    print("\nFINAL PATH:")
+    for nodes in solution_breadth.path():
+        print(nodes.state)
+
+    solution_uniform = uniform_cost_search(UniformCostRomania("Arad", base_tree, "Bucharest"))
+    # print(solution_uniform.path)
+
+    print("\nFINAL PATH:")
+    for nodes in solution_uniform.path():
+        print(nodes.state)
